2fb74fb1f44674db12dfdffefb98aefda38a9070
Jamie Throup <jamiethroup@gmail.com>
2021-09-18 23:07:50 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Base msg="Tell Me Jokes"/>
+</template>
+
+<script>
+import Base from './components/Base.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Base,
+  },
+};
+</script>
+
+<style lang="stylus">
+#app
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
---
+++ src/components/Base.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="absolute top-0 left-0 w-full h-full">
+  <div class="flex items-center justify-center h-full w-full">
+    <div class="container mx-auto">
+      <div class="grid grid-col-12">
+        <div class="col-span-12">
+          <h1 class="font-heading text-5xl text-red-400">{{ msg }}</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Base',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+h3
+  margin 40px 0 0
+
+ul
+  list-style-type none
+  padding 0
+
+li
+  display inline-block
+  margin 0 10px
+
+a
+  color #42b983
+</style>
