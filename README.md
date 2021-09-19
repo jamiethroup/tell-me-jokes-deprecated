29631b2f46602dbb79080ea54016b3bb3691a8e1
Jamie Throup <jamiethroup@gmail.com>
2021-09-19 10:44:51 +0100

    filters are now working

--- README.md
+++ README.md
@@ -1,24 +1,46 @@
-# tell-me-jokes
+# Joke Generator fueled by JokeAPI.dev
 
-## Project setup
-```
+
+
+## 🖥 System Requirements
+
+- [Node](https://nodejs.org/en/) (v14.7.5) ([NVM](https://github.com/nvm-sh/nvm) recommended)
+- [NPM 7+](https://docs.npmjs.com/try-the-latest-stable-version-of-npm)
+
+
+
+## 🎬 Getting Started
+
+1. Clone/download this repository.
+2. Using your preferred CLI, navigate to the directory root. Run `npm install` to install the needed packages
+4. Rum `npm run serve`
+
+
+
+#### Project setup
+
+```javascript
 npm install
 ```
 
-### Compiles and hot-reloads for development
-```
+#### Compiles and hot-reloads for development
+
+```javascript
 npm run serve
 ```
 
-### Compiles and minifies for production
-```
+#### Compiles and minifies for production
+
+```javascript
 npm run build
 ```
 
-### Lints and fixes files
-```
+#### Lints and fixes files
+
+```javascript
 npm run lint
 ```
 
-### Customize configuration
+#### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/components/Base.vue
+++ src/components/Base.vue
@@ -1,10 +1,28 @@
 <template>
 <main class="bg-gray-100">
-  <div class="flex items-center justify-center h-screen w-full">
-    <div class="w-1/4">
-      <div class="joke-wrap" v-if="joke">
+  <div class="flex items-center justify-center h-screen w-full flex-col">
+    <img width="250" src="../assets/logo.svg" alt="">
+    <div class=" max-w-full md:max-w-half w-screen p-4 bg-white rounded-xl shadow-md">
+    <label v-for="item in availableFilters" :key="item" :for="item">{{ item }}
+        <input class="form-tick appearance-none h-6 w-6 border border-gray-300 rounded-md
+      checked:bg-blue-600 checked:border-transparent focus:outline-none"
+          :id="item"
+          :value="item"
+          :name="item"
+          type="checkbox"
+          v-model="checkedFilters">
+    </label>
+      <div v-if="joke.joke">
         <div class="location-box">
-          <div class="location">{{ joke.category }}</div>
+          Single Joke
+          <h2 class="font-base text-3xl">{{ joke.joke }}</h2>
+        </div>
+      </div>
+      <div v-if="joke.setup">
+        <div class="location-box">
+          Two Part
+          <h2 class="font-base text-3xl">{{ joke.setup }}</h2>
+          <h2 class="font-base text-xl">{{ joke.delivery }}</h2>
           <div class="date">{{ dateBuilder() }}</div>
         </div>
       </div>
@@ -12,10 +30,9 @@
         <i class="fas fa-city absolute top-4 left-4"></i>
         <input
           type="text"
-          clhjjass="text-gray-400 placeholder-gray-400 py-3 pl-12 pr-5 outline-none border
+          class="text-gray-400 placeholder-gray-400 py-3 pl-12 pr-5 outline-none border
           rounded-md hover:rounded-md focus:rounded-md w-full"
           placeholder="Enter a City..."
-          v-model="query"
         />
         <button
           class="bg-red-500 block py-5 px-10 rounded-full uppercase font-bold
@@ -34,20 +51,22 @@ export default {
     return {
       url_base: 'https://v2.jokeapi.dev/joke/Any',
       query: '',
+      availableFilters: ['nsfw', 'religious', 'political', 'racist', 'sexist', 'explicit'],
+      checkedFilters: [],
       joke: {
       },
     };
   },
   methods: {
     fetchJoke() {
-      // fetch(`${this.url_base}?number=${this.query}`)
-      fetch(`${this.url_base}`)
+      fetch(`${this.url_base}?number=1&blacklistFlags=${this.checkedFilters}`)
         .then((res) => res.json())
         .then(this.setResults);
     },
+    updateFilters() {
+    },
     setResults(results) {
       this.joke = results;
-      console.log(this.joke.type);
     },
     dateBuilder() {
       const d = new Date();
